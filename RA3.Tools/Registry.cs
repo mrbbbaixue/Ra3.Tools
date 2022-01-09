714790ad1d06d66e82575f3db0c67682c1f68176
richcfno1 <chaifu2020@163.com>
2022-01-09 17:41:12 -0500

    Update Registry.cs

--- RA3.Tools/Registry.cs
+++ RA3.Tools/Registry.cs
@@ -4,16 +4,37 @@ namespace RA3.Tools
 {
     public static class Registry
     {
-        public static bool IsRegistryExist()
+        public enum RegistryStatus
+        {
+            Correct,
+            NotExist,
+            MissingPath,
+            MissingMapSync
+        }
+
+        public static RegistryStatus Status
+        {
+            get { return IsRegistryValid(); }
+        }
+
+        private static RegistryStatus IsRegistryValid()
         {
             using var view32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
             using var ra3 = view32.OpenSubKey("Software\\Electronic Arts\\Electronic Arts\\Red Alert 3", writable: true);
-            return ra3 != null;
-        }
-
-        public static int IsRegistryValid()
-        {
-            return 0;
+            if (ra3 == null)
+            {
+                return RegistryStatus.NotExist;
+            }
+            using var newra3 = view32.CreateSubKey("Software\\Electronic Arts\\Electronic Arts\\Red Alert 3", writable: true);
+            if (newra3.GetValue("Install Dir") == null)
+            {
+                return RegistryStatus.MissingPath;
+            }
+            if (newra3.GetValue("UseLocalUserMap") == null || (int)newra3.GetValue("UseLocalUserMap") != 0)
+            {
+                return RegistryStatus.MissingMapSync;
+            }
+            return RegistryStatus.Correct;
         }
 
         public static string GetRA3Path()
@@ -30,10 +51,10 @@ namespace RA3.Tools
             if (ra3 == null)
             {
                 using var newra3 = view32.CreateSubKey("Software\\Electronic Arts\\Electronic Arts\\Red Alert 3", writable: true);
-                newra3.SetValue("UseLocalUserMap", 0, RegistryValueKind.DWord);
+                newra3.SetValue("Install Dir", path, RegistryValueKind.String);
                 return;
             }
-            ra3.SetValue("UseLocalUserMap", 0, RegistryValueKind.DWord);
+            ra3.SetValue("Install Dir", path, RegistryValueKind.String);
         }
 
         public static void EnableMapSync()
