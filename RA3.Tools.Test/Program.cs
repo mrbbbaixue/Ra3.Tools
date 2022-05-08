cd5ab5a36321b98f279dafdf25b2efe07e3a3d4f
MrBBBaiXue <mrbbbaixue@bakneko.com>
2022-05-09 00:36:16 +0800

    Update Program.cs

--- RA3.Tools.Test/Program.cs
+++ RA3.Tools.Test/Program.cs
@@ -6,3 +6,7 @@ foreach (var i in ra3.Profiles)
     Console.WriteLine(i);
 }
 Console.WriteLine(ra3.GetCurrentProfile());
+
+Console.WriteLine("RA3 Path: " + RA3.Tools.Registry.GetRA3Path());
+Console.WriteLine("Registry Status: " + RA3.Tools.Registry.Status);
+Console.ReadKey();
